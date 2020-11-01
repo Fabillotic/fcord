7a58c43c0f428c6cbc9eddc168735ad67803258e
Fabillotic <fabillo552@gmail.com>
2020-11-01 12:32:10 +0100

    Removed annoying outputs.

--- mods/simple_command.py
+++ mods/simple_command.py
@@ -1,19 +1,12 @@
 import fcord
 
 def event(e):
-	if(e["t"].lower() == "message_create"):
-		if(e["d"]["content"].startswith("!")):
-			print("(SIMPLE) Command!")
+	if e["t"].lower() == "message_create":
+		if e["d"]["content"].startswith("!"):
 			command(e["d"]["content"][1:], e["d"]["channel_id"])
-		else:
-			print("(SIMPLE) Message not command!")
-	else:
-		print("(SIMPLE) Discarded event!")
 
 def command(c, channel):
 	s = c.split(" ")
-	if(s[0].lower() == "test"):
+	if s[0].lower() == "test":
 		print("(SIMPLE) testing...")
 		print(fcord.call("POST", "/channels/" + channel + "/messages", data='{"content": "TEST!"}', headers={"Content-Type": "application/json"}).text)
-	else:
-		print("(SIMPLE) Invalid command!")
