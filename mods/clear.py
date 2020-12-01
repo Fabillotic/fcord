fe20dfdb727c4149a09bb42114c5fad9884511d4
Fabillotic <fabillo552@gmail.com>
2020-12-01 16:16:37 +0100

    Fixed spacing.

--- mods/clear.py
+++ mods/clear.py
@@ -9,9 +9,9 @@ def event(e):
 		if e["d"]["content"].startswith("!clear"):
 			if fcord.has_permission(e["d"]["guild_id"], e["d"]["member"]["roles"], 13):
 				r = request("GET", "https://discord.com/api/v8/channels/" + e["d"]["channel_id"] + "/messages", headers={"User-Agent": fcord.user_agent, "Authorization": "Bot " + fcord.token})
-
+				
 				remove = []
-
+				
 				for m in r.json():
 					delta = (isoparse(m["timestamp"]) - timedelta(weeks=2)).timestamp()
 					if not delta < 0:
