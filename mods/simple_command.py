b441fc96bb895283825de8a802ada4ccacf28e50
Fabillotic <fabillo552@gmail.com>
2020-12-01 16:18:45 +0100

    Added cookie button!

--- mods/simple_command.py
+++ mods/simple_command.py
@@ -1,12 +1,17 @@
 import fcord
+import util
 
 def event(e):
 	if e["t"].lower() == "message_create":
 		if e["d"]["content"].startswith("!"):
-			command(e["d"]["content"][1:], e["d"]["channel_id"])
+			command(e["d"]["content"][1:], e["d"]["channel_id"], e["d"]["author"]["id"])
 
-def command(c, channel):
+def command(c, channel, user):
 	s = c.split(" ")
 	if s[0].lower() == "test":
 		print("(SIMPLE) testing...")
-		fcord.send("TEST!", channel)
+		j = fcord.send("TEST!", channel).json()
+		util.button(0x1F36A, j["id"], channel, back, [], [user])
+
+def back(e, args):
+	fcord.send("click!", e["d"]["channel_id"])
