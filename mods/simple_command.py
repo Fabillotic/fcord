d18c8cedfab528ab79a26317501370ea4cf95cf6
Fabillotic <fabillo552@gmail.com>
2020-12-03 20:23:55 +0100

    Changed indents from tabs to 4 spaces.

--- mods/simple_command.py
+++ mods/simple_command.py
@@ -2,16 +2,16 @@ import fcord
 import util
 
 def event(e):
-	if e["t"].lower() == "message_create":
-		if e["d"]["content"].startswith("!"):
-			command(e["d"]["content"][1:], e["d"]["channel_id"], e["d"]["author"]["id"])
+    if e["t"].lower() == "message_create":
+        if e["d"]["content"].startswith("!"):
+            command(e["d"]["content"][1:], e["d"]["channel_id"], e["d"]["author"]["id"])
 
 def command(c, channel, user):
-	s = c.split(" ")
-	if s[0].lower() == "test":
-		print("(SIMPLE) testing...")
-		j = fcord.send("TEST!", channel).json()
-		util.button(0x1F36A, j["id"], channel, back, [], [user])
+    s = c.split(" ")
+    if s[0].lower() == "test":
+        print("(SIMPLE) testing...")
+        j = fcord.send("TEST!", channel).json()
+        util.button(0x1F36A, j["id"], channel, back, [], [user])
 
 def back(e, args):
-	fcord.send("click!", e["d"]["channel_id"])
+    fcord.send("click!", e["d"]["channel_id"])
