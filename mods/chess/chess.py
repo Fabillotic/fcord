d209d625b476235f8534b4b378da33ccee3f6af8
Fabillotic <fabillo552@gmail.com>
2020-12-09 13:17:14 +0100

    Improved spacing.

--- mods/chess/chess.py
+++ mods/chess/chess.py
@@ -25,7 +25,7 @@ def move(e, move, player, state, preview):
 
 
 def send_board(state, channel):
-    board = "  1 2 3 4 5 6 7 8\n"
+    board = "     " + "1   " + chr(int("2009", 16)) + "2   " + chr(int("2009", 16)) + "3   " + chr(int("2009", 16)) + "4   " + chr(int("2009", 16)) + "5   " + chr(int("2009", 16)) + "6   " + chr(int("2009", 16)) + "7   " + chr(int("2009", 16)) + "8\n"
     for r in range(7, -1, -1):
         board += chr(97 + r) + " "
         for c in range(0, 8):
@@ -40,29 +40,29 @@ def get_piece_char(p):
     if not p:
         return chr(int("2b1c", 16))
     elif p == "RW":
-        return chr(int("2656", 16))
+        return chr(int("2009", 16)) + chr(int("2656", 16)) + chr(int("2009", 16))
     elif p == "PW":
-        return chr(int("2659", 16))
+        return chr(int("2009", 16)) + chr(int("2659", 16)) + chr(int("2009", 16))
     elif p == "PB":
         return chr(int("265F", 16))
     elif p == "RB":
-        return chr(int("265C", 16))
+        return chr(int("2009", 16)) + chr(int("265C", 16)) + chr(int("2009", 16))
     elif p == "NW":
-        return chr(int("2658", 16))
+        return chr(int("2009", 16)) + chr(int("2658", 16)) + chr(int("2009", 16))
     elif p == "NB":
-        return chr(int("265E", 16))
+        return chr(int("2009", 16)) + chr(int("265E", 16)) + chr(int("2009", 16))
     elif p == "BW":
-        return chr(int("2657", 16)) + chr(int("2009", 16))
+        return " " + chr(int("200A", 16)) + chr(int("2657", 16)) + chr(int("2009", 16))
     elif p == "BB":
-        return chr(int("265D", 16)) + chr(int("2009", 16))
+        return " " + chr(int("200A", 16)) + chr(int("265D", 16)) + chr(int("2009", 16))
     elif p == "QW":
-        return chr(int("2655", 16))
+        return chr(int("2009", 16)) + chr(int("2655", 16)) + chr(int("2009", 16))
     elif p == "QB":
-        return chr(int("265B", 16))
+        return chr(int("2009", 16)) + chr(int("265B", 16)) + chr(int("2009", 16))
     elif p == "KW":
-        return chr(int("2654", 16))
+        return chr(int("2009", 16)) + chr(int("2654", 16)) + chr(int("2009", 16))
     elif p == "KB":
-        return chr(int("265A", 16))
+        return chr(int("2009", 16)) + chr(int("265A", 16)) + chr(int("2009", 16))
 
 def send_help(c):
     fcord.send("Can't help ya right now.", c)
