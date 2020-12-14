019e3fe55e1d43a2ec69bf4408aca83dc21fb372
Fabillotic <fabillo552@gmail.com>
2020-12-14 08:15:41 +0100

    Implemented chess.

---
+++ mods/chess/chess_mod.py
@@ -0,0 +1,102 @@
+import fcord
+from fcord import relative as rel
+import util
+import chess_mod
+from requests import request
+import convert
+import os
+from PIL import Image
+import chess
+from chess import Board, Move
+
+def register():
+    util.register_game("chess", chess_mod.get_moves(), chess_mod.move, chess_mod.get_default(), chess_mod.send_help)
+
+def init():
+    if not os.path.exists(rel("mods/chess/images/chess-rook_white.png")): #Just a basic check
+        convert.start()
+
+    chess_mod.images["board"] = Image.open(rel("mods/chess/images/board.png"))
+    chess_mod.images["b"] = Image.open(rel("mods/chess/images/chess-bishop_black.png"))
+    chess_mod.images["B"] = Image.open(rel("mods/chess/images/chess-bishop_white.png"))
+    chess_mod.images["k"] = Image.open(rel("mods/chess/images/chess-king_black.png"))
+    chess_mod.images["K"] = Image.open(rel("mods/chess/images/chess-king_white.png"))
+    chess_mod.images["n"] = Image.open(rel("mods/chess/images/chess-knight_black.png"))
+    chess_mod.images["N"] = Image.open(rel("mods/chess/images/chess-knight_white.png"))
+    chess_mod.images["p"] = Image.open(rel("mods/chess/images/chess-pawn_black.png"))
+    chess_mod.images["P"] = Image.open(rel("mods/chess/images/chess-pawn_white.png"))
+    chess_mod.images["q"] = Image.open(rel("mods/chess/images/chess-queen_black.png"))
+    chess_mod.images["Q"] = Image.open(rel("mods/chess/images/chess-queen_white.png"))
+    chess_mod.images["r"] = Image.open(rel("mods/chess/images/chess-rook_black.png"))
+    chess_mod.images["R"] = Image.open(rel("mods/chess/images/chess-rook_white.png"))
+
+def get_default():
+    return chess.STARTING_FEN
+
+def get_moves():
+    x = []
+    for y in range(1, 9):
+        a = chr(96 + y)
+        for z in range(1, 9):
+            x.append(a + str(z))
+    return [x, x]
+
+def move(e, move_in, player, state, preview):
+    board = Board(state)
+    
+    if preview:
+        send_board(board, e["d"]["channel_id"])
+        return
+    
+    m = move_in[0] + move_in[1]
+    move = Move.from_uci(m)
+
+    if not move in board.legal_moves:
+        fcord.send("Invalid move!", e["d"]["channel_id"])
+        return
+
+    board.push(move)
+    send_board(board, e["d"]["channel_id"])
+    
+    if board.is_game_over():
+        if board.is_checkmate():
+            fcord.send("CHECKMATE!", e["d"]["channel_id"])
+        elif board.is_stalemate():
+            fcord.send("Stalemate.", e["d"]["channel_id"])
+        elif board.is_insufficient_material():
+            fcord.send("Insufficient material!", e["d"]["channel_id"])
+        fcord.send("Game over! Result: " + board.result(), e["d"]["channel_id"])
+        return board.fen(), True
+
+    return board.fen(), False
+
+chess_mod.images = {}
+
+def send_board(board, channel):
+    img = chess_mod.images["board"].copy()
+
+    for i in range(64):
+        x = (i // 8) * 512 + 256
+        y = (i % 8) * 512 + 256
+
+        p = board.piece_map().get(i)
+        if not p:
+            continue
+        p = chess_mod.images[p.symbol()]
+        img.paste(p, (x, y), p)
+    
+    o = rel("mods/chess/images/out.png")
+    img.save(o)
+    img.close()
+
+    f = open(o, "rb")
+    d = f.read()
+    f.close()
+
+    r = request("POST", "https://discord.com/api/channels/" + channel + "/messages", headers={"Authorization": "Bot " + fcord.token, "User-Agent": fcord.user_agent}, files={"img.png": d, "payload_json": (None, '{"content": "Board:", "embed": {"image": {"url": "attachment://img.png"}}}'.encode("utf-8"))})
+    print(r.text)
+
+    os.remove(o)
+
+def send_help(c):
+    fcord.send("Can't help ya right now.", c)
---
+++ mods/chess/convert.py
@@ -0,0 +1,55 @@
+from PIL import Image
+from PIL.ImageDraw import Draw
+from svglib.svglib import svg2rlg as svg
+from reportlab.graphics.renderPM import drawToFile as render
+import os
+from fcord import relative as rel
+
+def start():
+    print("Converting images...")
+    pieces = ["images/chess-bishop.svg", "images/chess-king.svg", "images/chess-knight.svg", "images/chess-pawn.svg", "images/chess-queen.svg", "images/chess-rook.svg"]
+    _p = []
+
+    for p in pieces:
+        _p.append(rel("mods/chess/" + p))
+
+    pieces = _p
+
+    for p in pieces:
+        s = svg(p)
+        b = p[:p.rfind(".")]
+        f = b + ".png"
+        render(s, f, fmt="PNG", bg=0x00ff00)
+        i = Image.open(f)
+        i = i.convert("RGBA")
+        l = i.load()
+        d = Draw(i)
+        
+        h = []
+        
+        for y in range(i.size[1]):
+            for x in range(i.size[0]):
+                if l[x, y][1] > 10:
+                    l[x, y] = (0, 0, 0, 0)
+                else:
+                    h.append((x, y))
+        
+        darken = lambda c, a: tuple([min(max(x - a, 0), 255) for x in c])
+        cs = [(darken((53, 46, 36, 255), -20), "black"), (darken((231, 201, 137, 255), -20), "white")]
+        for x in range(len(cs)):
+            c = cs[x]
+            print(c)
+            border = []
+            for n in h:
+                l[n[0], n[1]] = c[0]
+            ni = Image.new("RGBA", (512, 512), (0, 0, 0, 0))
+            ri = i.resize((i.size[0] * 3 // 4, i.size[1] * 3 // 4), Image.NEAREST)
+            pp = ((512 - ri.size[0]) // 2, (512 - ri.size[1]) // 2)
+            ni.paste(ri, pp, ri)
+            ni.save(b + "_" + c[1] + ".png")
+
+        os.remove(f)
+
+if __name__ == "__main__":
+    print("CONVERTING FROM MAIN!")
+    start()
